d9ead2324284ea31b4477d6cc02f5baf91bde315
ADirin <33372535+ADirin@users.noreply.github.com>
2023-07-28 08:33:38 +0000

    Dictionary of jobs created as a JSON file and called through HTML .

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-#@ is a decorator which indicates after backslash display hello world
+
+#Create a list of jobs and then add the list to the render as a parameter and finally modified the HTML in to do section
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Software Engineer',
+    'location':  'Espoo, FIN',
+    'salary': '€ 4000'
+  },
+  {
+    'id': 2,
+    'title': 'Python Programmer',
+    'location':  'TRE, FIN',
+    'salary': '€ 4500'
+  },
+  {
+    'id': 3,
+    'title': 'UX designer',
+    'location':  'HKI, FIN',
+    'salary': '€ 3500'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Eng',
+    'location':  'US',
+   }
+
+  
+]
+
+
+#@ is a decorator who indicates after the backslash display hello world
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', 
+                           jobs=JOBS,
+                          company_name='Amirs')
   #by running nothing has happened since in flash explained how things should be done
 
 print(__name__)
